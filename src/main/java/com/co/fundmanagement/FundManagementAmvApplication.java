34658f24788c502a7a6d9d47a3791a66571217c7
AldahirM <amvelasquez9621@gmail.com>
2024-10-05 09:45:55 -0500

    initial project

---
+++ src/main/java/com/co/fundmanagement/FundManagementAmvApplication.java
@@ -0,0 +1,13 @@
+package com.co.fundmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundManagementAmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundManagementAmvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/fundmanagement/FundManagementAmvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.fundmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundManagementAmvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
